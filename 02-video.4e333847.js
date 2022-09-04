b2c0daeeb0f245f3f01c8a1636bb86946ff9e485
Valeria-Stetsiuk <107633057+Valeria-Stetsiuk@users.noreply.github.com>
2022-09-04 18:26:50 +0000

    Deploying to gh-pages from @ Valeria-Stetsiuk/goit-js-hw-08@1767e8cf474716be918aa112a2837efa593dfc81 🚀

--- 02-video.4e333847.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);console.log(e),console.log(o)}();
-//# sourceMappingURL=02-video.4e333847.js.map
--- 02-video.dd56f8e1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);console.log(e),console.log(o);
-//# sourceMappingURL=02-video.dd56f8e1.js.map
